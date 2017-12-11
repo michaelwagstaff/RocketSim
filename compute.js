0202d307257af23e2e85c6ce68d5972cd3eb9477
Michael Wagstaff <michaelwagstaff2000@gmail.com>
2017-12-11 14:24:01 +0000

    Added air resistance modelling

--- compute.js
+++ compute.js
@@ -58,7 +58,28 @@ function gravity(rocket, relativeGravity)
 {
 	return rocket.currMass * 9.81 * relativeGravity;
 }
-
+function airResistance(rocket)
+{
+	var standardPressure = 101.325e3;
+	var standardTemp = 288.15;
+	var surfaceGravity = 9.8067;
+	var gasConstant = 8.31447;
+	var tempLapseRate = 0.0065;
+	var massOfAir = 0.02896;
+	var pressure;
+	var density = 0;
+	var airResistance = 0;
+	var airResistanceArray = [];
+	var total = Math.sqrt(Math.pow(rocket.vVelocity, 2) + Math.pow(rocket.hVelocity, 2));
+	//Declare an array
+	pressure = standardPressure * Math.pow(1 - (tempLapseRate * rocket.height) / standardTemp, (surfaceGravity * massOfAir) / (gasConstant * tempLapseRate));
+	//console.log("Air Pressure: {0}", pressure);
+	var specificGasConstant = gasConstant / massOfAir;
+	density = pressure / (specificGasConstant * 273); //Using 273 in place of current temperature
+	airResistance = rocket.coefficientOfDrag * density * Math.pow(total, 2) * Math.PI * Math.pow(5.2, 2) / 2;
+	airResistanceArray.push(Math.cos(rocket.rotation) * airResistance);
+	airResistanceArray.push(Math.sin(rocket.rotation) * airResistance);
+}
 function main()
 {
 	console.log("Stuff");
@@ -104,7 +125,7 @@ function stableOrbit(orbit, rocket, planet, frequencyOfCalc)
 			var relativeGravity = 1 - (rocket.hVelocity / idealVelocity)
 
 			//Calculate theta here based on parameters
-
+			airResistance(rocket);
 
 			//console.log(idealVelocity);
 			var resultantUp = (Math.cos(rocket.rotation) * rocket.thrust[0] * 1000) - gravity(rocket, relativeGravity);
